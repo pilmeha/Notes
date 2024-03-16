c11dafde387c5dd85117cdacbf42cfaf24572d1f
garma <garma@VARGAR>
2024-03-16 10:21:44 +0500

    Добавьте файлы проекта.

---
+++ Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://youtu.be/YJHzoLBLY8g?si=KIwLRARGjFKoJ931";
+    public string Message => "Это приложение для написания записей и заметок.\nА если нажать на кнопку, то вы перейдете на прикольный плейлист песень \"Russian Circles\".";
+}
